119fef685768144d3eef59356f9549f61a48e94d
Adeoye <adeolaabeni88@gmail.com>
2022-09-13 09:09:01 -0700

    task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int round = 1;
 	char ch;
 
-	for (round = 1; round <= 10; round ++)
+	for (round = 1; round <= 10; round++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
