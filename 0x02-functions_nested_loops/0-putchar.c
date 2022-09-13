31e7b899c56ba28b7d98f973b0c7c3864826c519
Adeoye <adeolaabeni88@gmail.com>
2022-09-13 07:19:21 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
-include "main.h"
+#include "main.h"
 
 /**
- * main - prints Main
+ * main - prints putchar
  *
- * Description: prints Main
+ *Description: prints _putchar
  *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char text[10] = "Main";
-	int i = 0;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
