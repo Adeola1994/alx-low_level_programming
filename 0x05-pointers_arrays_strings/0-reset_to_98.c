fb9e45a78a1351a8a7632c78ddcf34ef0be8728f
Adeoye <adeolaabeni88@gmail.com>
2022-09-19 11:27:49 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 _ a function that takes a pointer
+ * reset_to_98 -  a function that takes a pointer
  * and updates the value it points to
  * @n: pointer
  * Return: void
