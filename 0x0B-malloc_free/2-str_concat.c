d76a42b7f2fc884b78d15936835319e3c999a984
Adeoye <adeolaabeni88@gmail.com>
2022-10-04 10:21:27 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,15 +28,15 @@ char *str_concat(char *s1, char *s2)
 
 	if (strout == NULL)
 	{
-		free (strout);
+		free(strout);
 		return (NULL);
 	}
 
-	for (k =0; k < i; k++)
+	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
 
 	limit = j;
-	for ( j = 0; j <= limit; k++, j++)
+	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
 
 	return (strout);
