ac16e060229e162752239d4f1d7e8747ae8a066f
Adeoye <adeolaabeni88@gmail.com>
2022-10-06 10:13:13 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,13 +3,13 @@
 
 /**
  * string_nconcat - concatenates two strings using at
- * most inputted number of bytes
+ * most an  inputted number of bytes
  * @s1: The first string
  * @s2: The second string
  * @n: The maximum number of bytes of s2 to concatenate to s1
  *
  * Return: if the function fails - NULL.
- * otherwise: a pointer to the concatenated space in memory
+ * otherwise - a pointer to the concatenated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
