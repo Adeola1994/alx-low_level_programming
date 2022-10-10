1c6a96010c428d6f671a73cef4438e9352db3fce
Adeoye <adeolaabeni88@gmail.com>
2022-10-10 12:05:57 -0700

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,7 +28,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (i = 0, 1 ; 1++)
+	for (i = 0, 1 ; i++)
 	{
 		(*(newDog->name + i)) = name[i];
 		if (name[i] == '\0')
