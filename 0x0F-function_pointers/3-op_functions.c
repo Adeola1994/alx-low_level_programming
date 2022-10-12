9946b8b9411c43559fa91155b2403491c03835d1
Adeoye <adeolaabeni88@gmail.com>
2022-10-12 15:19:26 -0700

    task3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -30,7 +30,7 @@ int op_sub(int a, int b)
  * op_mul - product of two numbers
  * @a: first integer
  * @b: second integer
- * Return: product(integer)
+ * Return: product (integer)
  */
 
 int op_mul(int a, int b)
@@ -67,9 +67,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
+		exit(100);
 	}
-
-	return (a % b);
+		return (a % b);
 }
-
-
