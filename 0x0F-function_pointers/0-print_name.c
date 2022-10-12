7534f387cebe0f0fec1bec2a64e47469e3e8a251
Adeoye <adeolaabeni88@gmail.com>
2022-10-12 12:14:20 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <function_pointers.h>
+
+/**
+ * print_name - a function that print a name
+ * @name: the name to be printed
+ * @f: a pointer to a function that accepts a string as
+ * parameter and returns nothing
+ * Return: returns nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
