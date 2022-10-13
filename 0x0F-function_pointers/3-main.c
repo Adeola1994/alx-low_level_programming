361881d0d231e6b4fc21a54f3022675e714771ca
Adeoye <adeolaabeni88@gmail.com>
2022-10-13 12:31:53 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,13 +4,12 @@
 #include "3-calc.h"
 
 /**
- * main - a program that performs simple arithmetic 
- * operations by calling helper functions
- * @argc: the number of command-line arguments
- * @argv: array of strings containing cmd-line arguments
- * Return: returns 0 (success)
- */
-
+* main - a program that performs simple arithmetic operations by calling
+* helper functions
+* @argc: the number of command-line arguments
+* @argv: an array of strings containing one cmd-line argument per string
+* Return: returns 0 (success)
+*/
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
@@ -23,8 +22,14 @@ int main(int argc, char *argv[])
 	}
 
 	if (strlen(argv[2]) == 1 && (argv[2][0] == '+' || argv[2][0] == '-'
-			|| argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
+		|| argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
 	{
+		if (atoi(argv[3]) == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+		{
+			printf("Error\n");
+			exit(100);
+		}
+
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[3]);
 
