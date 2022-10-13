2ec90b084523e6e001d5985bfc89b42897f8ef66
Adeoye <adeolaabeni88@gmail.com>
2022-10-13 01:55:48 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,11 +25,11 @@ int main(int argc, char *argv[])
 	if (strlen(argv[2]) == 1 && (argv[2][0] == '+' || argv[2][0] == '-'
 			|| argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
 	{
-		if (atoi(argv[3]) == 0 || (argv[2][0] == '/' || argv[2][0] == '%'))
+		/*if (atoi(argv[3]) == 0 || (argv[2][0] == '/' || argv[2][0] == '%'))
 		{
 			printf("Error\n");
 			exit(100);
-		}
+		}*/
 
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[3]);
