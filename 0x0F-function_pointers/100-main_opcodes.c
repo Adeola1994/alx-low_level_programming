cb96e926c60c63ae10812fe6b14fd879a0d3bce1
Adeoye <adeolaabeni88@gmail.com>
2022-10-13 03:18:00 -0700

    tadk4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
- /**
-  * main - a program that prints its own opcodes
-  * @argc: number of command-line arguments
-  * @argv: array of strings that contain each  command-line arguments
-  * Return: returns 0 (Success)
-  */
-int main(int argc, char *argv[])
- {
-         int i, n;
-         char *str;
+/**
+ * main - a program that prints its own opcodes
+ * @argc: number of command-line arguments
+ * @argv: array of strings that contain each command-line arguements
+ * Return: returns 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, n;
+	char *str;
 
-         if (argc != 2)
-         {
-                 printf("Error\n");
-                 exit(1);
-         }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-         n = atoi(argv[1]);
-         if (n < 0)
-         {
-                 printf("Error\n");
-                 exit(2);
-         }
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-         str = (char *) main;
-         for (i = 0; i < n; i++)
-         {
-                 if (i == n - 1)
-                 {
-                         printf("%02hhx\n", str[i]);
-                         break;
-                 }
-                 printf("%02hhx ", str[i]);
-         }
-		return (0);
- }
+	str = (char *)main;
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%2hhx\n" , str[i]);
+			break;
+		}
+		printf("%2hhx\n" , str[i]);
+	}
+	return (0);
+}
+                 
